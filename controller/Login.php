0357a0b2d23c82d419994660006f27d021fee832
SaraPristovnik <pristovniksara106@gmail.com>
2022-01-20 19:50:13 +0100

    Minor changes

--- controller/Login.php
+++ controller/Login.php
@@ -9,10 +9,10 @@ $error = [];
 
 if (!empty($_POST)) {
 
-    $userEmail = $_POST['email'] ?? '';
-    $userPassword = $_POST['password'] ?? '';
+    $userEmail = $_POST['email'];
+    $userPassword = $_POST['password'];
 
-    $user = Users::findUser($userEmail, $userPassword);
+    $user = Users::findUser($userEmail, $userPassword); // ce ni uporabnika v bazi, NULL (not set)
 
     if (isset($user)) {
         $_SESSION['userId'] = $user->id;
